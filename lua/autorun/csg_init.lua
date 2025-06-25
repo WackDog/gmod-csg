a43696dc2176f59ce76cdabc2361701382244dce
Jack Williams <jackw2503@outlook.com>
2025-06-25 20:04:03 +0100

    Pathing Correction and Commenting

--- lua/autorun/csg_init.lua
+++ lua/autorun/csg_init.lua
@@ -2,11 +2,17 @@
 -- Loads CSG server and client modules
 
 if SERVER then
-    include("csg/sh_config.lua")
-    include("csg/sv_cameras.lua")
-    AddCSLuaFile("csg/sh_config.lua")
-    AddCSLuaFile("csg/cl_viewer.lua")
+   -- Send entity files to clients
+   AddCSLuaFile("entities/ent_csg_camera/shared.lua")
+   AddCSLuaFile("entities/ent_csg_camera/cl_init.lua")
+   AddCSLuaFile("entities/ent_csg_terminal/shared.lua")
+   AddCSLuaFile("entities/ent_csg_terminal/cl_init.lua")
+
+   -- Load server-side modules
+   include("csg/sh_config.lua")
+   include("csg/sv_cameras.lua")
 else
-    include("csg/sh_config.lua")
-    include("csg/cl_viewer.lua")
+   -- Clients get config and UI
+   include("csg/sh_config.lua")
+   include("csg/cl_viewer.lua")
 end
